c7df1ddccecae094260cdedb45529ed9fbfcdb1a
mosesomondi-1 <mosesomondi137@gmail.com>
2023-07-18 05:21:16 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - a program that prints _putchar
+ * Followed by a new line
+ * Return: 0 (success)
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
