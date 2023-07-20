85229af970660e9a2015e12df5389e483c8cce6c
mosesomondi-1 <mosesomondi137@gmail.com>
2023-07-20 09:51:56 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "man.h"
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ * Return: Numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+int n;
+
+for (n = 0; n <= 9; n++)
+{
+_putchar(x + '0');
+}
+_putchar('\n');
+}
